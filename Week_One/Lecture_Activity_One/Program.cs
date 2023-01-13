ce17bc5a2ca8db72e2e6b508e2924cd7c68d3262
Sean Berrios <berrios96sean@gmail.com>
2023-01-12 22:15:10 -0700

    Added required functionality

--- Week_One/Lecture_Activity_One/Program.cs
+++ Week_One/Lecture_Activity_One/Program.cs
@@ -15,6 +15,12 @@ namespace Lecture_Activity_One
             Console.WriteLine("Please enter a Country Name: ");
             countryName = Console.ReadLine();
             String isoCode = mySvr.CountryISOCode(countryName);
+            String capitolCity = mySvr.CapitalCity(isoCode);
+            ServiceReference1.tCurrency curr = mySvr.CountryCurrency(isoCode);
+            Console.WriteLine("ISO Code = {0}",isoCode);
+            Console.WriteLine("Country Capitol = {0}", capitolCity);
+            Console.WriteLine("Currency ISO Code = {0}", curr.sISOCode);
+            Console.WriteLine("Currency Name = {0}",curr.sName);
         }
     }
 }
