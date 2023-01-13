7a9f2079dd0db9ea6058b491fea360d7e82b027a
Sean Berrios <berrios96sean@gmail.com>
2023-01-12 22:25:25 -0700

    Added Format String Function

--- Week_One/Lecture_Activity_One/Program.cs
+++ Week_One/Lecture_Activity_One/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Lecture_Activity_One
 {
@@ -14,6 +15,7 @@ namespace Lecture_Activity_One
             String countryName;
             Console.WriteLine("Please enter a Country Name: ");
             countryName = Console.ReadLine();
+            countryName = FormatString(countryName);
             String isoCode = mySvr.CountryISOCode(countryName);
             String capitolCity = mySvr.CapitalCity(isoCode);
             ServiceReference1.tCurrency curr = mySvr.CountryCurrency(isoCode);
@@ -22,5 +24,14 @@ namespace Lecture_Activity_One
             Console.WriteLine("Currency ISO Code = {0}", curr.sISOCode);
             Console.WriteLine("Currency Name = {0}",curr.sName);
         }
+
+        // This function will format the string to capitolize the leading characted and remove any trailing whitespace 
+        // so that unecessary user errors do not occur. 
+        public static string FormatString(string input)
+        {
+            input = input.TrimEnd(); // remove trailing whitespace
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input);
+        }
+
     }
 }
