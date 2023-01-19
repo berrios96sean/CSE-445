1a72b507d94680186b95e376d5609806292e28f7
Sean Berrios <berrios96sean@gmail.com>
2023-01-19 13:24:03 -0700

    testing sumofdigits implementation

--- Week_Two/Lecture_Activity/LectureActivityTwoBerriosSean/consoleClientApp/Program.cs
+++ Week_Two/Lecture_Activity/LectureActivityTwoBerriosSean/consoleClientApp/Program.cs
@@ -10,15 +10,40 @@ namespace consoleClientApp
     {
         static void Main(string[] args)
         {
-            while(true)
-            {
-                Console.WriteLine("Welcome to Web Service Demo!");
+            ServiceReference1.Service1Client mySvc = new ServiceReference1.Service1Client();
+            Console.WriteLine("******************************");
+            Console.WriteLine("Welcome to Web Service Demo!");
 
-                if (Console.Read() == 3)
+            while (true)
+            {
+                Console.WriteLine("******************************");
+                Console.WriteLine("Select the method you need to invoke:");
+                Console.WriteLine("1. Sum of Digits");
+                Console.WriteLine("2. Vowel Count");
+                Console.WriteLine("3. EXIT"); 
+                string selection = Console.ReadLine(); 
+
+                if (selection.Equals("1"))
                 {
-                    Environment.Exit(0); 
+                    Console.WriteLine("******************************");
+                    Console.WriteLine("Please enter an integer number: ");
+                    string intNum = Console.ReadLine();
+                    int x = Convert.ToInt32(intNum);
+                    int res = mySvc.sumOfDigits(x);
+                    Console.WriteLine("Result = {0}",res);
+                    Console.WriteLine("******************************");
                 }
+                if (selection.Equals("2"))
+                {
+                    Console.WriteLine("2 pressed");
+                }
+                if (selection.Equals("3"))
+                {
+                    break; 
+                }
+
             }
+
         }
     }
 }
