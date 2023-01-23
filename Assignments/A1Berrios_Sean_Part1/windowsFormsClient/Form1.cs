f41bd3aa167bd42ba16e500b05218363cd9d3c0a
Sean Berrios <berrios96sean@gmail.com>
2023-01-22 22:45:50 -0700

    implemented gui and proj creation

---
+++ Assignments/A1Berrios_Sean_Part1/windowsFormsClient/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsFormsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client mySvc = new ServiceReference1.Service1Client();
+            string upper, lower, digits, vowels;
+            int words;
+            words = mySvc.wordCount(textBox1.Text);
+            upper = mySvc 
+        }
+    }
+}
