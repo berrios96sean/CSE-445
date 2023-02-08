a89201f91ac538239d22367ac86db032d5c824a9
Sean Berrios <berrios96sean@gmail.com>
2023-02-08 16:39:43 -0700

    finished first two classes

--- Lecture_Activities/lecture_activity_4_sean_berrios/Program.cs
+++ Lecture_Activities/lecture_activity_4_sean_berrios/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace lecture_activity_4_sean_berrios
 {
@@ -10,6 +10,64 @@ namespace lecture_activity_4_sean_berrios
     {
         static void Main(string[] args)
         {
+            // create an object instance of the string analyzer class 
+            // call the read string method of the string analyzer class 
+            // create three threads to count digits, count uppercase and determine 
+            // if the string is a palindrome 
+            // start threads 
+            // wait for all the threads to complete 
+            // display string analysis results 
         }
     }
+
+    #region String analyzer class 
+    class StringAnalyzer
+    {
+        String inputStr; 
+
+        public void anaylzeString(String str)
+        {
+            Console.WriteLine("Please Enter a String");
+            String input = Console.ReadLine();
+            this.inputStr = input; 
+        }
+
+        public string getInput()
+        {
+            return this.inputStr; 
+        }
+        
+       
+    }
+    #endregion
+
+    #region Digit Count Class 
+    class DigitCount  
+    {
+        int count = 0;
+        string str; 
+
+        public DigitCount(String str)
+        {
+            this.str = str; 
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    this.count++;
+                }
+            }
+        }
+
+        public int getCount()
+        {
+            return this.count;
+        }
+    }
+    #endregion
+
 }
