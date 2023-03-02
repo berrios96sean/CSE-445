3e0c5887c8513d061f64c8352f10ae3908ba16b6
Sean Berrios <berrios96sean@gmail.com>
2023-03-02 11:17:55 -0700

    Initial creation of practice for A2

---
+++ Assignments/A2Working_DIR/A2Practice_Book_2_6_3_Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading; 
+using System.Threading.Tasks;
+
+namespace A2Practice_Book_2_6_3_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
