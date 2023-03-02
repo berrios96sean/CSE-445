054cc7b8dd5d30a1bd71f8d8ef207401a2a3ffa7
Sean Berrios <berrios96sean@gmail.com>
2023-03-02 13:46:13 -0700

    created project skeleton

--- Assignments/A2Working_DIR/A2Berrios_Sean/Program.cs
+++ Assignments/A2Working_DIR/A2Berrios_Sean/Program.cs
@@ -6,10 +6,87 @@ using System.Threading.Tasks;
 
 namespace A2Berrios_Sean
 {
+    #region Airline class
+    class Airline
+    {
+
+    }
+
+    #endregion
+
+    #region Pricing Model Class
+
+    class PricingModel
+    {
+
+    }
+
+    #endregion
+
+    #region Order Processing Class
+
+    class OrderProcessing
+    {
+
+    }
+
+    #endregion
+
+    #region Travel Agency 
+
+    class TravelAgency
+    {
+
+    }
+
+    #endregion
+
+    #region Order Class
+
+    class OrderClass
+    {
+
+    }
+
+    #endregion
+
+    #region Multi Cell Buffer 
+
+    class MultiCellBuffer
+    {
+
+    }
+
+    #endregion
+
+    #region Encoder Class
+
+    class Encoder
+    {
+
+    }
+
+    #endregion
+
+    #region Decoder Class 
+
+    class Decoder
+    {
+
+    }
+
+    #endregion
+
+    #region Main Program 
+
     class Program
     {
         static void Main(string[] args)
         {
         }
     }
+
+    #endregion
+
+
 }
