452923fd2e326cd0d121291d559df39e439b04a8
Sean Berrios <berrios96sean@gmail.com>
2023-03-02 11:26:23 -0700

    defining a delegate

--- Assignments/A2Working_DIR/A2Practice_Book_2_6_3_Example/Program.cs
+++ Assignments/A2Working_DIR/A2Practice_Book_2_6_3_Example/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 
 namespace A2Practice_Book_2_6_3_Example
 {
+
+    // Define a delegate that represents the signature of methods that can handle a price cut event with an Int32 parameter.
+    // This allows us to reference any number of methods that match this signature and handle price cut events with different price cuts,
+    // without needing to define multiple event handlers with similar but slightly different signatures.
+    public delegate void priceCutEvent(Int32 pr); 
     class Program
     {
         static void Main(string[] args)
