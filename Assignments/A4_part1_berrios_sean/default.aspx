6f34614f43273386fcc58b7762980b0ff836e963
Sean Berrios <berrios96sean@gmail.com>
2023-04-15 22:41:12 -0700

    finished

--- Assignments/A4_part1_berrios_sean/default.aspx
+++ Assignments/A4_part1_berrios_sean/default.aspx
@@ -9,8 +9,43 @@
 <body>
     <form id="form1" runat="server">
         <div>
-            Hello
+            <asp:Label ID="Label9" runat="server" Text="<span style='font-weight:bold;'>xml url:</span> http://localhost:57279/Restaurants.xml"></asp:Label>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Enter Xml URL:  "></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server" Width="221px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Load" />
+            <br />
+            <br />
+            <asp:Label ID="Label7" runat="server" Text="Select a Restaurant to View Info  then click 'info' button"></asp:Label>
+            <p>
+                <strong>
+                <asp:DropDownList ID="DropDownList1" runat="server">
+                </asp:DropDownList>
+                </strong>
+            </p>
+            <p>
+                <asp:Button ID="Button2" runat="server" OnClick="Button2_Click1" Text="Info" />
+            </p>
         </div>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Restaurant Name:  "></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server" Width="211px"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label3" runat="server" Text="Phone:  "></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server" Width="157px"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Email:  "></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server" Width="159px"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label5" runat="server" Text="Website:  "></asp:Label>
+        <asp:TextBox ID="TextBox5" runat="server" Width="249px"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label6" runat="server" Text="Facebook URL:  "></asp:Label>
+            <asp:TextBox ID="TextBox6" runat="server" Width="224px"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label8" runat="server" Text="Delivers:  "></asp:Label>
+        <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
     </form>
 </body>
 </html>
--- Assignments/A4_part1_berrios_sean/default.aspx.cs
+++ Assignments/A4_part1_berrios_sean/default.aspx.cs
@@ -4,14 +4,140 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Net;
 
 namespace A4_part1_berrios_sean
 {
     public partial class _default : System.Web.UI.Page
     {
+        public XmlDocument doc = new XmlDocument();
+        public XmlNode node;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                   | SecurityProtocolType.Tls11
+                   | SecurityProtocolType.Tls12
+                   | SecurityProtocolType.Ssl3;
+
+            string file = TextBox1.Text;
+            doc.Load(file);
+
+            XmlNodeList restaurantList = doc.SelectNodes("//Restaurants/restaurant");
+
+            List<string> names = new List<string>();
+            List<string> delivery = new List<string>();
+            List<string> phone = new List<string>();
+            List<string> email = new List<string>();
+            List<string> website = new List<string>();
+            List<string> facebook = new List<string>();
+            foreach (XmlNode restaurantNode in restaurantList)
+            {
+                string deliver_y = restaurantNode.Attributes["Delivery"].Value;
+                delivery.Add(deliver_y);
+                string name = restaurantNode.SelectSingleNode("name").InnerText;
+                names.Add(name);
+                string phon_e = restaurantNode.SelectSingleNode("contact/phone").InnerText;
+                phone.Add(phon_e);
+                string emai_l = restaurantNode.SelectSingleNode("contact/email").InnerText;
+                email.Add(emai_l);
+                string websit_e = restaurantNode.SelectSingleNode("website").InnerText;
+                website.Add(websit_e);
+                string faceboo_k = restaurantNode.SelectSingleNode("facebook").InnerText;
+                facebook.Add(faceboo_k);
+            }
+           
+
+            for(int i = 0; i < names.Count; i++)
+            {
+                DropDownList1.Items.Add(names[i]);
+
+            }
+            
+        }
+
+
+        protected void Button2_Click1(object sender, EventArgs e)
+        {
+            string file = TextBox1.Text;
+            doc.Load(file);
+
+            XmlNodeList restaurantList = doc.SelectNodes("//Restaurants/restaurant");
+
+            List<string> names = new List<string>();
+            List<string> delivery = new List<string>();
+            List<string> phone = new List<string>();
+            List<string> email = new List<string>();
+            List<string> website = new List<string>();
+            List<string> facebook = new List<string>();
+            foreach (XmlNode restaurantNode in restaurantList)
+            {
+                string deliver_y = restaurantNode.Attributes["Delivery"].Value;
+                delivery.Add(deliver_y);
+                string name = restaurantNode.SelectSingleNode("name").InnerText;
+                names.Add(name);
+                string phon_e = restaurantNode.SelectSingleNode("contact/phone").InnerText;
+                phone.Add(phon_e);
+                string emai_l = restaurantNode.SelectSingleNode("contact/email").InnerText;
+                email.Add(emai_l);
+                string websit_e = restaurantNode.SelectSingleNode("website").InnerText;
+                website.Add(websit_e);
+                string faceboo_k = restaurantNode.SelectSingleNode("facebook").InnerText;
+                facebook.Add(faceboo_k);
+            }
+
+            if (DropDownList1.SelectedValue == names[0])
+                TextBox2.Text = names[0];
+            TextBox3.Text = phone[0];
+            TextBox4.Text = email[0];
+            TextBox5.Text = website[0];
+            TextBox6.Text = facebook[0];
+            TextBox7.Text = delivery[0];
+            {
+
+            }
+            if (DropDownList1.SelectedValue == names[1])
+            {
+                TextBox2.Text = names[1];
+                TextBox3.Text = phone[1];
+                TextBox4.Text = email[1];
+                TextBox5.Text = website[1];
+                TextBox6.Text = facebook[1];
+                TextBox7.Text = delivery[1];
+            }
+            if (DropDownList1.SelectedValue == names[2])
+            {
+                TextBox2.Text = names[2];
+                TextBox3.Text = phone[2];
+                TextBox4.Text = email[2];
+                TextBox5.Text = website[2];
+                TextBox6.Text = facebook[2];
+                TextBox7.Text = delivery[2];
+            }
+            if (DropDownList1.SelectedValue == names[3])
+            {
+                TextBox2.Text = names[3];
+                TextBox3.Text = phone[3];
+                TextBox4.Text = email[3];
+                TextBox5.Text = website[3];
+                TextBox6.Text = facebook[3];
+                TextBox7.Text = delivery[3];
+            }
+            if (DropDownList1.SelectedValue == names[4])
+            {
+                TextBox2.Text = names[4];
+                TextBox3.Text = phone[4];
+                TextBox4.Text = email[4];
+                TextBox5.Text = website[4];
+                TextBox6.Text = facebook[4];
+                TextBox7.Text = delivery[4];
+            }
         }
     }
 }
