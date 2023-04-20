49918ab8aa0d3d498efdff96b6dffabdd8c19a51
Sean Berrios <berrios96sean@gmail.com>
2023-04-19 21:42:03 -0700

    implemented verification

--- Assignments/A4part_2_berrios_sean/Service1.svc.cs
+++ Assignments/A4part_2_berrios_sean/Service1.svc.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace A4part_2_berrios_sean
 {
@@ -12,22 +15,26 @@ namespace A4part_2_berrios_sean
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string verification(string xml, string xsd)
         {
-            return string.Format("You entered: {0}", value);
+            try
+            {
+                XmlDocument schemaDoc = new XmlDocument();
+                schemaDoc.Load(xsd);
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+                schemaSet.Add(XmlSchema.Read(new StringReader(schemaDoc.OuterXml), null));
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xml);
+                xmlDoc.Schemas = schemaSet;
+                xmlDoc.Validate(null);
+
+                return "No Error";
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
     }
 }
